ebf3e401df027b5f6dd8c73acf08613ffe975c12
Valuh-Marko <mvaluh@gmail.com>
2023-03-03 00:12:36 +0100

    Fixed up the banner animations and implmented text switch

--- src/components/Banner/banner.js
+++ src/components/Banner/banner.js
@@ -1,4 +1,5 @@
-import React, { useContext, useEffect } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import React, { useContext, useEffect, useState } from "react";
 import GeneralContext from "../../context/GeneralContext";
 import { Divider } from "../Decoratives/divider";
 import { Logo } from "../Icons/Logo/logo";
@@ -6,15 +7,44 @@ import "./banner.scss";
 
 export const Banner = () => {
   const { latinSayings } = useContext(GeneralContext);
-  const saying = latinSayings[Math.floor(Math.random() * latinSayings.length)];
+  const [isVisible, setVisible] = useState(true);
+  const [saying, setSaying] = useState(
+    latinSayings[Math.floor(Math.random() * latinSayings.length)]
+  );
+
+  useEffect(() => {
+    const setInvisibleInterval = setInterval(() => {
+      setVisible(false);
+    }, 4000);
+    setTimeout(() => {
+      const setVisibleInterval = setInterval(() => {
+        setVisible(true);
+        setSaying(
+          latinSayings[Math.floor(Math.random() * latinSayings.length)]
+        );
+      }, 4000);
+    }, 1000);
+  }, []);
 
   return (
     <>
       <div className="c-banner">
         <Logo customClass="c-banner__logo" />
-        <h2 className="c-heading">{saying.latin}</h2>
-        <Divider />
-        <p className="c-subheading">{saying.translate}</p>
+        <AnimatePresence>
+          {isVisible ? (
+            <motion.div
+              key="nekiKey"
+              className="c-banner__text-holder"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.8 }}
+            >
+              <motion.h2 className="c-heading">{saying.latin}</motion.h2>
+              <motion.p className="c-subheading">{saying.translate}</motion.p>
+            </motion.div>
+          ) : null}
+        </AnimatePresence>
       </div>
     </>
   );
--- src/components/Icons/Logo/logo.js
+++ src/components/Icons/Logo/logo.js
@@ -10,48 +10,48 @@ export const Logo = ({ customClass }) => {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g clip-path="url(#clip0_177_48)">
+      <g clipPath="url(#clip0_177_48)">
         <path
           d="M50.59 463.26L50.58 463.25V463.26H50.59Z"
           fill="currentColor"
           stroke="currentColor"
-          stroke-miterlimit="10"
+          strokeMiterlimit="10"
         />
         <path
           d="M200.85 550.01L200.84 550V550.01H200.85Z"
           fill="currentColor"
           stroke="currentColor"
-          stroke-miterlimit="10"
+          strokeMiterlimit="10"
         />
         <path
           d="M301.02 550.01V550L301.01 550.01H301.02Z"
           fill="currentColor"
           stroke="currentColor"
-          stroke-miterlimit="10"
+          strokeMiterlimit="10"
         />
         <path
           d="M501.36 145.16C488.88 137.95 463.76 123.45 451.28 116.25C428.96 103.38 398.46 85.75 376.15 72.87V506.63C398.46 493.75 428.96 476.12 451.28 463.26C463.76 456.05 488.88 441.55 501.36 434.34V318.67C488.88 311.46 463.76 296.96 451.28 289.75C463.76 282.54 488.88 268.04 501.36 260.83V145.16ZM451.28 405.42L426.24 419.88H426.23V333.12H426.24L451.28 347.58V405.42ZM451.28 231.92L426.24 246.38H426.23V159.62L451.28 174.08V231.92Z"
           fill="currentColor"
           stroke="currentColor"
-          stroke-miterlimit="10"
+          strokeMiterlimit="10"
         />
         <path
           d="M301.02 29.49H301.01C288.53 22.28 263.41 7.77999 250.93 0.579987C238.45 7.77999 213.33 22.28 200.85 29.49H200.84C188.36 36.7 163.24 51.2 150.76 58.41V521.09C163.24 528.3 188.36 542.8 200.84 550V87.32L225.89 72.87V564.47L250.93 578.92L275.97 564.47V72.87L301.02 87.32V550C313.5 542.8 338.62 528.3 351.1 521.09V58.41C338.62 51.2 313.5 36.7 301.02 29.5V29.49Z"
           fill="currentColor"
           stroke="currentColor"
-          stroke-miterlimit="10"
+          strokeMiterlimit="10"
         />
         <path
           d="M75.63 217.46L50.58 203V116.25C38.1 123.45 12.98 137.95 0.5 145.16V434.34C12.98 441.55 38.1 456.05 50.58 463.25V231.92L75.62 246.38H75.63V477.71C88.11 484.92 113.23 499.42 125.71 506.63V72.87C113.23 80.08 88.11 94.58 75.63 101.79V217.46Z"
           fill="currentColor"
           stroke="currentColor"
-          stroke-miterlimit="10"
+          strokeMiterlimit="10"
         />
         <path
           d="M50.58 116.24V116.25L50.59 116.24H50.58Z"
           fill="currentColor"
           stroke="currentColor"
-          stroke-miterlimit="10"
+          strokeMiterlimit="10"
         />
       </g>
       <defs>
