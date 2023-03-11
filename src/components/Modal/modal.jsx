0a062e13b78f81064c34ed9543e51618c8c90afd
Valuh-Marko <mvaluh@gmail.com>
2023-03-11 17:23:41 +0100

    Implemented Mobile Friendly Modals

--- src/components/Modal/modal.jsx
+++ src/components/Modal/modal.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { motion } from 'framer-motion'
 import { Backdrop } from '../Backdrop/backdrop'
 import ReactDOM from 'react-dom'
 import ornament35 from "../../assets/images/ornament35.svg";
 import ornament16 from "../../assets/images/ornament16.svg";
+import close from "../../assets/images/close.svg"
 import "./modal.scss";
 
 export const Modal = ({ handleClose, data }) => {
-
   return ReactDOM.createPortal(
     <Backdrop onClick={handleClose}>
       <motion.div
         onClick={(e) => e.stopPropagation()}
         className="c-modal"
       >
+        <img src={close} alt="close-img" className='c-close-icon' onClick={handleClose} />
         <img src={data.img} alt="image" className="c-modal-img" />
         <div className="c-modal-text-container">
           <h2 className="c-modal-title">{data.title}</h2>
           <p className="c-modal-desc">{data.desc}</p>
-          <img src={ornament16} alt="ornament" className="c-ornament-bottom" />
         </div>
+        <img src={ornament16} alt="ornament" className="c-ornament-bottom" />
         <img src={ornament35} alt="ornament" className="c-ornament-top" />
       </motion.div>
     </Backdrop>,
